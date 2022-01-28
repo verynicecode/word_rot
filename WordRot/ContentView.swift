8a7cce4535ef1121206bbeeace054646eeeed010
Jon Allured <jon.allured@gmail.com>
2022-01-28 14:01:27 -0600

    Initial Commit

---
+++ WordRot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordRot
+//
+//  Created by Jonathan Allured on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordRot/WordRotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordRotApp.swift
+//  WordRot
+//
+//  Created by Jonathan Allured on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WordRotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
