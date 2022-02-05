5efcfead70f0d41babae449d4af048299cb611cc
Jon Allured <jon.allured@gmail.com>
2022-02-05 10:05:34 -0600

    User plays word

--- WordRot/ContentView.swift
+++ WordRot/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var word: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 20) {
+            TextField("", text:$word)
+                .textFieldStyle(.roundedBorder)
+            Button(action: playWord) {
+                Text("PLAY")
+            }
+            .buttonStyle(.bordered)
+            .foregroundColor(.black)
+        }
+        .padding(20)
+    }
+    
+    func playWord() {
+        word = ""
     }
 }
 
