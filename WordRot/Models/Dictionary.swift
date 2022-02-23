8f112081877405292e66a5c1a5786f2935cef55a
Jon Allured <jon.allured@gmail.com>
2022-02-23 12:19:22 -0600

    Use a real dictionary

---
+++ WordRot/Models/Dictionary.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SQLite
+
+class Dictionary {
+    static let shared = Dictionary()
+    
+    private let client: Connection
+    
+    init() {
+        let location = Bundle.main.path(forResource: "words.sqlite3", ofType: nil)!
+        let client = try! Connection(location)
+        self.client = client
+    }
+    
+    func isValid(_ word: String) -> Bool {
+        let query = "select count(*) from words where word = '\(word)'"
+        let count = try! client.scalar(query) as! Int64
+        return count == 1
+    }
+}
