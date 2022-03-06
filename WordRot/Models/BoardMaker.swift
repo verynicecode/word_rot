54d90f07ceeb713f310f80eb0f592694a29e49fd
Jon Allured <jon.allured@gmail.com>
2022-03-06 12:37:12 -0600

    Extract class for filling board
    This improves the distribution quite a bit but I've ignored the larger
    problems around rotting the board after each play. We'll get there!

---
+++ WordRot/Models/BoardMaker.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+class BoardMaker {
+    static func fill() -> [[LetterPair]] {
+        let rows = 5
+        let columns = 5
+        
+        let totalLetters = rows * columns
+
+        let maker = BoardMaker(totalLetters: totalLetters)
+        maker.fill()
+        let letters = maker.letters
+        
+        let data: [[LetterPair]] = Array(0..<rows).map { row in
+            return Array(0..<columns).map { column in
+                let index = (row * rows) + column
+                return letters[index]
+            }
+        }
+        
+        return data
+    }
+    
+    let totalLetters: Int
+    
+    var vowels = [
+        "a", "a", "e", "e", "e", "i", "i", "o", "o", "u"
+    ]
+    
+    var consonants = [
+        "b", "c", "d", "f", "g", "h",
+        "j", "k", "l", "m", "n",
+        "p", "q", "r", "s", "t",
+        "v", "w", "x", "y", "z"
+    ]
+    
+    var letters: [LetterPair] = []
+    
+    init(totalLetters: Int) {
+        self.totalLetters = totalLetters
+    }
+    
+    func fill() {
+        addVowels()
+        addDupes()
+        addConsonants()
+        shuffleLetters()
+    }
+    
+    func addVowels() {
+        let vowelCount = Int.random(in: 3...6)
+        
+        for _ in 1...vowelCount {
+            let letter = vowels.remove(at: Int.random(in: 0..<vowels.count))
+            letters.append((letter, 2))
+        }
+    }
+    
+    func addDupes() {
+        let dupeCount = Int.random(in: 1...3)
+        
+        for _ in 1...dupeCount {
+            let letter = consonants.remove(at: Int.random(in: 0..<consonants.count))
+            letters.append((letter, 2))
+            letters.append((letter, 2))
+        }
+    }
+    
+    func addConsonants() {
+        let remainingCount = totalLetters - letters.count
+        
+        for _ in 1...remainingCount {
+            let letter = consonants.remove(at: Int.random(in: 0..<consonants.count))
+            letters.append((letter, 2))
+        }
+    }
+    
+    func shuffleLetters() {
+        letters.shuffle()
+    }
+}
--- WordRot/Models/LetterBoard.swift
+++ WordRot/Models/LetterBoard.swift
@@ -2,29 +2,9 @@ import Foundation
 
 typealias LetterPair = (String, Int)
 
-let alphabet = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-]
-
 class LetterBoard {
-    static func makeLetterPair() -> LetterPair {
-        let initialRot = Int.random(in: 0...5)
-        let initialLetterIndex = Int.random(in: 0...25)
-        let initialLetter = alphabet[initialLetterIndex]
-        
-        return (initialLetter, initialRot)
-    }
-    
     static func start() -> LetterBoard {
-        let rows = 5
-        let columns = 5
-        
-        let data: [[LetterPair]] = Array(1...rows).map { _ in
-            return Array(1...columns).map { _ in
-                return makeLetterPair()
-            }
-        }
+        let data = BoardMaker.fill()
         
         let letterRows: [LetterRow] = data.map { letters in
             let tiles = letters.map() { letterPair in
