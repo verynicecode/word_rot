7a59f57c841f2437d27891535ab2e65970028d7f
Jon Allured <jon.allured@gmail.com>
2022-03-20 14:47:44 -0500

    Copy initial database during loading

--- WordRot/Models/Loader.swift
+++ WordRot/Models/Loader.swift
@@ -1,7 +1,13 @@
 import Foundation
+import SQLite
 
 struct Loader {
     static func check(pass passCallback: () -> Void, fail failCallback: () -> Void) {
+        databaseStuff()
+        killswitchStuff(pass: passCallback, fail: failCallback)
+    }
+    
+    static func killswitchStuff(pass passCallback: () -> Void, fail failCallback: () -> Void) {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         
@@ -15,8 +21,22 @@ struct Loader {
         
         let buildVersion = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
         let buildNumber = Int(buildVersion)!
-                
+        
         let callback = killswitch.verify(buildNumber) ? passCallback : failCallback
         callback()
+    }
+    
+    static func databaseStuff() {
+        let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let destinationPath = documentsUrl.appendingPathComponent("rotten.sqlite3").path
+        
+        guard !FileManager.default.fileExists(atPath: destinationPath) else { return }
+        
+        do {
+            let sourcePath = Bundle.main.path(forResource: "rotten.sqlite3", ofType: nil)!
+            try FileManager.default.copyItem(atPath: sourcePath, toPath: destinationPath)
+        } catch {
+            print("error during file copy: \(error)")
+        }
     }
 }
