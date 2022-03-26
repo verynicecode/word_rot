7103cfd594de4b7d6c70d8dd328fb9c5ca022412
Jon Allured <jon.allured@gmail.com>
2022-03-26 10:37:49 -0500

    When bailing be sure to invoke pass callback

--- WordRot/Models/Loader.swift
+++ WordRot/Models/Loader.swift
@@ -17,7 +17,7 @@ struct Loader {
             let killswitchUrl = URL(string: endpoint),
             let data = try? Data(contentsOf: killswitchUrl),
             let killswitch = try? decoder.decode(Killswitch.self, from: data)
-        else { return }
+        else { passCallback(); return }
         
         let buildVersion = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
         let buildNumber = Int(buildVersion)!
