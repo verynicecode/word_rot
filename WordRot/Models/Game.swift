7c41b9127fc42f0f78f828cd73b130478297dc82
Jon Allured <jon.allured@gmail.com>
2022-03-26 10:34:28 -0500

    Persist game and round info to the database

--- WordRot/Models/Game.swift
+++ WordRot/Models/Game.swift
@@ -4,14 +4,26 @@ class Game: ObservableObject {
     @Published var lastError: String?
     @Published var score = 0
     
+    let id: Int
     let letterBoard = LetterBoard.start()
     
     var playedWords: [String] = []
+    var rounds: [Round] = []
+    
+    init() {
+        let sql = "insert into games(status) values('started') returning id;"
+        let id = try! RottenDB.sharedClient.scalar(sql) as! Int64
+        self.id = Int(id)
+    }
     
     func playWord(_ word: String) {
         guard !playedWords.contains(word) else { lastError = "word already played"; return }
         guard word.count > 3 else { lastError = "word too short"; return }
         guard Dictionary.isValid(word) else { lastError = "word not found"; return }
+        
+        let nextRoundNumber = playedWords.count + 1
+        let round = Round(gameId: id, word: word, number: nextRoundNumber)
+        rounds.append(round)
         
         playedWords.append(word)
         score += word.count
