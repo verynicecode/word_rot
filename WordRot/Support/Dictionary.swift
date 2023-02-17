26a019d07b5a833b5d6327e9726c75e66e9469ac
Jon Allured <jon.allured@gmail.com>
2023-02-16 21:50:43 -0600

    WIP

--- WordRot/Support/Dictionary.swift
+++ WordRot/Support/Dictionary.swift
@@ -4,7 +4,12 @@ import SQLite
 class Dictionary {
     static func isValid(_ word: String) -> Bool {
         let query = "SELECT COUNT(*) FROM words WHERE word = '\(word)';"
-        let count = try! RottenDB.sharedClient.scalar(query) as! Int64
+        
+        guard
+            let binding = try? RottenDB.sharedClient.scalar(query),
+            let count = binding as? Int64
+        else { return false }
+        
         return count == 1
     }
 }
